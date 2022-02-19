336a0a250a0fa00b27b40a6baf337bda0b20a6f0
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2022-02-19 20:19:31 +0200

    DrillApp commit

---
+++ Labor1/DrillApp/DrillApp/Drill.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillApp
+{
+    public enum Speed 
+    { 
+        Slow = 500,
+        Medium = 1350,
+        Fast = 1850
+    }
+
+    public enum Material
+    {
+        Metal,Plastic,Titanium
+    }
+
+    public class Drill
+    {
+        public List<string> IncludeComponents { get; set; }
+        public string InstallationMethod { get; set; }
+        public string ItemWeigth { get; set; }
+        public string Manufacturer { get; set; }
+        public Material Material { get; }
+        public IList<Material> Materials { get; set; }
+        public string Note { get; set; }
+        public string PackageDimensions { get; set; }
+        public string PartNumber { get; set; }
+        public string PowerSource { get; set; }
+        public string Size { get; set; }
+        public Speed Speed { get;}
+        public IList<Speed> Speeds { get; set; }
+        public string Torque { get; set; }
+        public Usage Usage { get;}
+    }
+}
---
+++ Labor1/DrillApp/DrillApp/Usage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillApp
+{
+    public class Usage
+    {
+        public bool Concrete { get; set; }
+        public bool HardBrick { get; set; }
+        public bool HardMaterial { get; set; }
+        public bool Metal { get; set; }
+        public bool Screwdriver { get; set; }
+        public bool Wood { get; set; }
+    }
+}
