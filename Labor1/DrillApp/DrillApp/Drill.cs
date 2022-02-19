a3148be692916932960dc2c67b8efb055e66796f
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2022-02-19 23:24:24 +0200

    DrillApp commit

--- Labor1/DrillApp/DrillApp/Drill.cs
+++ Labor1/DrillApp/DrillApp/Drill.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace DrillApp
 {
-    public enum Speed 
-    { 
+    public enum Speed
+    {
         Slow = 500,
         Medium = 1350,
         Fast = 1850
@@ -20,20 +20,34 @@ namespace DrillApp
 
     public class Drill
     {
-        public List<string> IncludeComponents { get; set; }
+        public List<string> IncludeComponents { get; set; } = new List<string>();
         public string InstallationMethod { get; set; }
         public string ItemWeigth { get; set; }
         public string Manufacturer { get; set; }
-        public Material Material { get; }
-        public IList<Material> Materials { get; set; }
+        public Material Material { get; set; }
+        public IList<Material> Materials { get; set; } = new List<Material>();
         public string Note { get; set; }
         public string PackageDimensions { get; set; }
         public string PartNumber { get; set; }
         public string PowerSource { get; set; }
         public string Size { get; set; }
-        public Speed Speed { get;}
-        public IList<Speed> Speeds { get; set; }
+        public Speed Speed { get; set; }
+        public IList<Speed> Speeds { get; set; } = new List<Speed>();
         public string Torque { get; set; }
-        public Usage Usage { get;}
+        public Usage Usage { get; set; } = new Usage();
+
+        public void loadSpeeds()
+        {
+            Speeds.Add(Speed.Slow);
+            Speeds.Add(Speed.Medium);
+            Speeds.Add(Speed.Fast);
+        }
+
+        public void loadMaterials()
+        {
+            Materials.Add(Material.Metal);
+            Materials.Add(Material.Plastic);
+            Materials.Add(Material.Titanium);
+        }
     }
 }
--- Labor1/DrillApp/DrillApp/MainWindow.xaml.cs
+++ Labor1/DrillApp/DrillApp/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace DrillApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Drill kimo = new Drill();
+        public Drill kimo;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,12 +28,62 @@ namespace DrillApp
 
         private void applyButton_Click(object sender, RoutedEventArgs e)
         {
+            kimo = new Drill
+            {
+                Manufacturer = "KIMO.",
+                PackageDimensions = "13.7 x 10.6 x 3 inches",
+                ItemWeigth = "2.84 pounds",
+                PartNumber = "K13811",
+                Size = "Medium",
+                PowerSource = "Battery Powered",
+                Torque = "350 Inch Pounds",
+                InstallationMethod = "Screw-In",
+                Note = "Average Battery Life 1000 Hours"
+            };
 
+            kimo.Usage.Concrete = true;
+            kimo.Usage.Wood = true;
+            kimo.Usage.Metal = true;
+            kimo.Usage.HardBrick = true;
+            kimo.Usage.HardMaterial = false;
+            kimo.Usage.Screwdriver = true;
+            kimo.IncludeComponents.Add("1xKIMO Cordless K13811 Drill Driver");
+            kimo.IncludeComponents.Add("1x2.0Ah 20V Lithium-Ion Battery");
+            kimo.IncludeComponents.Add("1xFast Charger");
+            kimo.loadMaterials();
+            kimo.loadSpeeds();
+
+            //kod szinten igy nezne ki(ennek alternativaja az XAML-ben a <Window.DataContext-nel> van,ahol megadtam egy instancet/hivatkozast a Drill osztalynak)
+            this.DataContext = kimo;
         }
 
         private void resetButton_Click(object sender, RoutedEventArgs e)
         {
+            kimo.Manufacturer = "";
+            kimo.PackageDimensions = "";
+            kimo.ItemWeigth = "";
+            kimo.PartNumber = "";
+            kimo.Size = "";
+            kimo.PowerSource = "";
+            kimo.Torque = "";
+            kimo.InstallationMethod = "";
+            kimo.Note = "";
+            kimo.Usage.Concrete = false;
+            kimo.Usage.Wood = false;
+            kimo.Usage.Metal = false;
+            kimo.Usage.HardBrick = false;
+            kimo.Usage.HardMaterial = false;
+            kimo.Usage.Screwdriver = false;
+            kimo.IncludeComponents.Clear();
+            kimo.Materials.Clear();
+            kimo.Speeds.Clear();
 
+            if(DataContext != null)
+            {
+                DataContext = null;
+            }
+
+            DataContext = kimo;
         }
     }
 }
