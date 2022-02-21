e08923ce3f34b85a7c93367c30302abaa05e6630
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2022-02-21 11:44:06 +0200

    Labor2 commit

---
+++ Labor2/ProductApp/ProductApp/Data/DrinkProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Data
+{
+    enum MaterialType
+    {
+        Granules,
+        Leaf,
+        Liquid,
+        Paste,
+        Powder,
+        Other
+    }
+
+    class DrinkProduct
+    {
+        public int AnnualSales { get; set; }
+        public MaterialType Material { get; set; } = new MaterialType();
+        public string PackageType { get; set; }
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Labor2/ProductApp/ProductApp/Helper/IntegerToBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿using ProductApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ProductApp.Helper
+{
+    //itt megmondjuk, hogy egy Int erteket egy Brushes ertekke alakitsa(hogy tudjam kiszinezni a szamokat)
+    [ValueConversion(typeof(int),typeof(Brushes))]
+    class IntegerToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DrinkProduct product = (DrinkProduct)value;
+
+            if(product.Quantity < 10000)
+            {
+                //itt ki kell szinezzem a Quantity int erteket pirosra
+                return new SolidBrush(Color.Red);
+            }
+
+            return new SolidBrush(Color.Blue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
