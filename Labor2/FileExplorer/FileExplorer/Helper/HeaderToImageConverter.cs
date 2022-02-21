d75960fe24e4c61b080a9fbad8896d7525c591ac
TNorbi <tasnadi.istvan@student.ms.sapientia.ro>
2022-02-21 11:00:01 +0200

    Labor2 commit

---
+++ Labor2/FileExplorer/FileExplorer/Helper/HeaderToImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace FileExplorer.Helper
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter headerToImageConverter = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = (string)value;
+            if (path == null)
+                return null;
+            string name = MainWindow.getFileFolderName(path);
+            string image = "Images/file.png";
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder.png";
+            return new BitmapImage(new Uri($"D:/EGYETEMI-DOLGOK/HARMAD_EV/FELEV2/C# .NET technologiak/Laborok/Labor2/FileExplorer/FileExplorer/{image}",UriKind.Absolute));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
